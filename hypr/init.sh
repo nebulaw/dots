6fb635a7c9037d473e6ee0eebc4b1010eb2edcb7
nebula <karlo.tsutskiridze@gmail.com>
2024-04-02 22:18:10 +0400

    refact(hypr): added handling for brightness in hyprland

--- hypr/init.sh
+++ hypr/init.sh
@@ -5,7 +5,7 @@ pkill swww
 pkill eww
 
 swww-daemon &
-swww img ~/Media/Pictures/Wallpapers/w5.png
+swww img ~/Media/Pictures/Wallpapers/w19.jpg
 eww daemon &
 eww -c ~/.config/eww open win --screen 0 &
 
