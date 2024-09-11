86e69651dd3061478ff314268244cfd0c74529d6
nebula <karlo.tsutskiridze@gmail.com>
2024-09-11 22:05:25 +0400

    minor changes with gaps and add georgian layout

--- hypr/init.sh
+++ hypr/init.sh
@@ -5,7 +5,7 @@ pkill swww
 pkill eww
 
 swww-daemon &
-swww img ~/Media/Pictures/Wallpapers/w19.jpg
+swww img ~/Media/Pictures/Wallpapers/debili.jpg
 eww daemon &
 eww -c ~/.config/eww open win --screen 0 &
 
